5a557a07a2c56e01a460486824fb8e49fcd6c637
Sergey Okishev <so@aspirity.com>
2022-01-27 21:36:22 +0700

    Fixed build error related to types. Added "any" to strange behaviour for dontCache policy for gql queries.

--- src/api/graphQL/apolloClient.ts
+++ src/api/graphQL/apolloClient.ts
@@ -1,12 +1,13 @@
 import { ApolloClient, HttpLink, InMemoryCache } from '@apollo/client';
 
 // since we are not using infinity load - we want to wipe all the previouse results and get only the new one
-const dontCache = () => {
+// TODO: something strange going on here. If you copy-paste result of this function as value for fields - it's fine. But getting this value as result of function (which is the same) - TS error
+const dontCache = (): any => {
   return {
     // Don't client separate results based on
     // any of this field's arguments.
     keyArgs: false,
-    merge(_: unknown, incoming: unknown[]) {
+    merge(_: unknown, incoming: unknown[]): unknown[] {
       // aggregations bypass
       if (!incoming?.length) return incoming;
 
