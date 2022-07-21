27331a19404f48745ebda1dad77b396fc286642d
Anna Mikhailova <amikhailova@usetech.com>
2022-07-21 15:11:18 +0300

    Revert "Merge pull request #134 from UniqueNetwork/epic/Scan_main_page_redesign"
    This reverts commit 601d92e29b76b5a82c07af745c9eb4f9f374b576, reversing
    changes made to 866e040f9b60da4fe590aacfad2f57112db9de47.

--- src/styles/styled-components/Header1.tsx
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-export const Header1 = styled.div`
-  font-family: 'Raleway';
-  font-style: normal;
-  font-weight: 700;
-  font-size: 36px;
-  line-height: 48px;
-`;
--- src/styles/styled-components/Header3.tsx
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-export const Header3 = styled.div`
-  font-family: 'Raleway';
-  font-style: normal;
-  font-weight: 700;
-  font-size: 24px;
-  line-height: 36px;
-`;
