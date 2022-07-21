3e57dcd7a66245007aea80b2978ffea79e5ff8af
a-mikhailova <95228032+a-mikhailova@users.noreply.github.com>
2022-07-21 13:18:42 +0300

    Merge pull request #134 from UniqueNetwork/epic/Scan_main_page_redesign
    [SCAN-277] Token information and statistics block

---
+++ src/styles/styled-components/BodyS.tsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const BodyS = styled.div`
+  font-family: 'Inter';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 14px;
+  line-height: 22px;
+`;
