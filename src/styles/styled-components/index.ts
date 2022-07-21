2bc7fa3324b4d47c26ef954482cc31be2f5652b1
Konstantin Pozdnikin <kpozdnikin@gmail.com>
2022-07-21 11:51:56 +0300

    [SCAN-280] Todo for headers added

--- src/styles/styled-components/index.ts
+++ src/styles/styled-components/index.ts
@@ -1,5 +1,6 @@
 export * from './BodyM';
 export * from './BodyS';
+// TODO - use UI kit headers after https://cryptousetech.atlassian.net/browse/UI-140 fix
 export * from './Header';
 export * from './Header3';
 export * from './Header4';
