f6013dac702dcbcf0bb4cf053bab0079b0705394
Anna Mikhailova <amikhailova@usetech.com>
2022-09-05 09:27:49 +0300

    check for external path

--- src/components/Icon/useGetIcon.tsx
+++ src/components/Icon/useGetIcon.tsx
@@ -4,13 +4,23 @@ export const useGetIcon = (path: string) => {
   const [icon, setIcon] = useState('');
 
   useEffect(() => {
-    fetch(path)
-      .then((response) => {
-        return response.text();
-      })
-      .then((data) => {
-        setIcon(data);
-      });
+    const isExternalLink = (url: string) => {
+      const tmp = document.createElement('a');
+      tmp.href = url;
+      return tmp.host !== window.location.host;
+    };
+    // For safety reasons, only internal paths are allowed
+    if (!isExternalLink(path)) {
+      fetch(path)
+        .then((response) => {
+          return response.text();
+        })
+        .then((data) => {
+          setIcon(data);
+        });
+    } else {
+      throw new Error('You need to specify the internal path for icons');
+    }
   }, [path]);
 
   return icon;
