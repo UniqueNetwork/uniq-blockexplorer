48acd94a3601985bc7e06fbf982f2f698dcb6817
Anna Mikhailova <amikhailova@usetech.com>
2022-11-16 12:42:58 +0300

    nk from bundle to the new page and fix filter on collection page

--- src/components/BundleCard.tsx
+++ src/components/BundleCard.tsx
@@ -1,92 +1,103 @@
-import { FC, useCallback } from 'react';
+import { FC } from 'react';
 import styled from 'styled-components/macro';
-import { Link, useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { Text } from '@unique-nft/ui-kit';
 
 import { useApi, useCheckImageExists } from '@app/hooks';
 import { timeDifference } from '@app/utils';
 import { Token } from '@app/api';
-import { UserEvents } from '@app/analytics/user_analytics';
-import { logUserEvents } from '@app/utils/logUserEvents';
 import { Picture } from '@app/components';
 import { SVGIcon } from '@app/components/SVGIcon';
 
 type BundleCardProps = Token & { timeNow?: number };
 
 const BundleCard: FC<BundleCardProps> = ({
+  bundle_created: bundleCreated,
+  children_count,
   collection_id: collectionId,
   collection_name: name,
-  bundle_created: bundleCreated,
   image,
   timeNow,
   token_id: tokenId,
   token_prefix: prefix,
-  children_count,
+  type,
   transfers_count,
 }) => {
-  const navigate = useNavigate();
   const { currentChain } = useApi();
-  // user analytics
-  const onNFTCardClick = useCallback(() => {
-    const path = window.location.pathname;
 
-    if (path.includes('collections')) {
-      logUserEvents(UserEvents.Click.ON_NFT_CARD_ON_COLLECTION_PAGE);
-    }
+  let typeLinkPart = type === 'FRACTIONAL' ? 'fractional' : 'nfts';
 
-    navigate(`/${currentChain.network.toLowerCase()}/nfts/${collectionId}/${tokenId}`);
-  }, [collectionId, currentChain.network, navigate, tokenId]);
+  const navigateTo = `/${currentChain.network.toLowerCase()}/${typeLinkPart}/${collectionId}/${tokenId}`;
 
   const { imgSrc } = useCheckImageExists(image.fullUrl);
 
   return (
-    <TokenCardLink onClick={onNFTCardClick}>
+    <BundleCardLink to={navigateTo}>
       {/* the picture has not exists */}
       {!imgSrc && <TokenPicture alt={tokenId.toString()} src={imgSrc} />}
       {/* the picture has loaded */}
       {imgSrc && <TokenBackground imgUrl={imgSrc} />}
       <TokenTitle>
         <Text color="primary-500" size="l">{`${prefix || ''} #${tokenId}`}</Text>
-        <div>
+        <CollectionLinkWrapper>
           <TokenCollectionLink
             to={`/${
               currentChain ? currentChain?.network + '/' : ''
             }collections/${collectionId}`}
           >
-            {name} [{collectionId}]
+            {name} [ID {collectionId}]
           </TokenCollectionLink>
-        </div>
+        </CollectionLinkWrapper>
         <TokenProperties>
-          <Text color="grey-500" size="xs">
-            Transfers:{' '}
-            <Text color="additional-dark" size="xs">
+          <TransfersRow>
+            <Text color="grey-500" size="xs" weight="light">
+              Transfers:&nbsp;
+            </Text>
+            <Text color="additional-dark" size="xs" weight="light">
               {transfers_count}
             </Text>
-          </Text>
-          <Text color="grey-500" size="xs">
-            Nested items:{' '}
-            <Text color="additional-dark" size="xs">
+          </TransfersRow>
+          <NestedRow>
+            <Text color="grey-500" size="xs" weight="light">
+              Nested items:&nbsp;
+            </Text>
+            <Text color="additional-dark" size="xs" weight="light">
               {children_count}
             </Text>
-          </Text>
-          <CreatedDate>
+          </NestedRow>
+          <CreatedRow>
             <StyledSVGIcon height={16} name="clock" width={16} />
-            <Text color="additional-dark" size="xs">
+            <Text color="additional-dark" size="xs" weight="light">
               {timeDifference(bundleCreated, timeNow)}
             </Text>
-          </CreatedDate>
+          </CreatedRow>
         </TokenProperties>
       </TokenTitle>
-    </TokenCardLink>
+    </BundleCardLink>
   );
 };
+
+const TokenProperties = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const NestedRow = styled.div`
+  display: flex;
+  margin-bottom: 2px;
+`;
+
+const TransfersRow = styled.div`
+  display: flex;
+  margin-bottom: 2px;
+`;
 
 const StyledSVGIcon = styled(SVGIcon)`
   color: var(--blue-gray-400);
   margin-right: 5px;
 `;
 
-const TokenCardLink = styled.div`
+const BundleCardLink = styled(Link)`
   cursor: pointer;
   width: 100%;
   border: 1px solid var(--blue-gray-200);
@@ -115,6 +126,10 @@ const TokenCollectionLink = styled(Link)`
   line-height: 18px;
 `;
 
+const CollectionLinkWrapper = styled.div`
+  margin-bottom: calc(var(--gap) / 2);
+`;
+
 const TokenBackground = styled.div<{ imgUrl: string }>`
   width: 100%;
   background-image: url(${(props) => props.imgUrl});
@@ -138,14 +153,7 @@ const TokenTitle = styled.div`
   }
 `;
 
-const TokenProperties = styled.div`
-  display: flex;
-  flex-direction: column;
-  margin-top: calc(var(--gap) / 2);
-  gap: 2px;
-`;
-
-const CreatedDate = styled.div`
+const CreatedRow = styled.div`
   display: flex;
   align-items: center;
 `;
